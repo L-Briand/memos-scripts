7a3d0562b9d5e05d07496f927e8746bc8c4cbad0
Lionel Briand <lionel.briand@orandja.fr>
2022-04-06 08:51:46 +0200

    Ubuntu pgadmin

---
+++ ubuntu-pgadmin.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+apt update
+apt upgrade
+apt install -y curl gnupg 
+
+curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | apt-key add
+echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update
+apt install -y pgadmin4-web 
+/usr/pgadmin4/bin/setup-web.sh
