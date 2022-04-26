3bd590b864b187ff89513c41d9f2e5ad94cb1c94
Lionel Briand <lionel.briand@partnre.com>
2022-04-26 16:35:22 +0200

    better filebrowser

--- alpine-filebrowser.sh
+++ alpine-filebrowser.sh
@@ -9,7 +9,19 @@ ln -s /usr/local/bin/filebrowser /usr/bin/filebrowser
 apk del bash curl
 
 # generate dir to store files
-mkdir -p /srv/files/public
+mkdir -p /etc/filebrowser/
+mkdir -p /srv/files/
+
+# init database
+filebrowser config init \
+    -d /etc/filebrowser/filebrowser.db \
+    -r /srv/files \
+    -a 0.0.0.0 \
+    -p 80 \
+    -l /var/run/filebrowser.log
+
+# Set user that can't be deleted because of id 1.
+filebrowser users add admin admin --perm.admin -d /etc/filebrowser/filebrowser.db 
 
 # create a service
 cat > /etc/init.d/filebrowser <<EOF 
@@ -17,10 +29,10 @@ cat > /etc/init.d/filebrowser <<EOF
 
 name="filebrowser"
 description=""
-pidfile="/run/${RC_SVCNAME}.pid"
+pidfile="/run/\${RC_SVCNAME}.pid"
 
 command="/usr/bin/filebrowser"
-command_args="-a 0.0.0.0 -p 80 -d /srv/files/filebrowser.db -r /srv/files/public/"
+command_args="-d /etc/filebrowser/filebrowser.db"
 command_background=true
 EOF
 
@@ -28,5 +40,5 @@ EOF
 chmod +x /etc/init.d/filebrowser
 rc-update add filebrowser default
 
-# startit
+# start it
 /etc/init.d/filebrowser start
