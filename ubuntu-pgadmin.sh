a13ec8346db3a1d2422ff5d0eb0a8620d0c32c13
Lionel Briand <lionel.briand@orandja.fr>
2022-05-08 10:40:07 +0200

    fix dependencies for ubuntu-pgadmin

--- ubuntu-pgadmin.sh
+++ ubuntu-pgadmin.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt update
 apt upgrade
-apt install -y curl gnupg 
+apt install -y curl gnupg lsb-release
 
 curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | apt-key add
 echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update
